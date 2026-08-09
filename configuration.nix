2aa3bf1d7c62d13255374929dd0b92b70e3da06a
vs-123 <>
2026-08-09 13:27:20 +0000

    replace vim with neovim

--- configuration.nix
+++ configuration.nix
@@ -75,7 +75,7 @@
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
-    vim
+    neovim
     wget
     git
     chezmoi
