bd6fcb0bacd115525db8cd5ea2276fc890440244
vs-123 <>
2026-08-10 09:17:05 +0000

    redeclare programs in home file

--- xe_home.nix
+++ xe_home.nix
@@ -1,9 +1,11 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [
-    kitty
-    neovim
-  ];
   home.stateVersion = "26.11";
 
-  programs.firefox.enable = true;
+  programs = {
+    firefox.enable = true;
+    neovim.enable = true;
+    kitty.enable = true;
+  };
+
+  home.packages = with pkgs; [ ];
 }
