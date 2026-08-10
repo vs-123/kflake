d98a4f7bb72dcf81df150eb08f971fe2df8db8e0
vs-123 <>
2026-08-10 06:27:11 +0000

    trying out home-manager

--- flake.nix
+++ flake.nix
@@ -3,21 +3,31 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
   };
 
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations = {
+  outputs = { self, nixpkgs, home-manager }: {
+    nixosConfigurations = 
+    let
+      common_modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          #home-manager.extraSpecialArgs = { inherit inputs; };
+          #home-manager.users.jdoe = ./home.nix;
+        }
+      ];
+    in
+    {
       karm = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
-        modules = [
-          ./configuration.nix
-        ];
+        modules = common_modules;
       };
       kx86 = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-        ];
+        modules = common_modules;
       };
     };
   };
