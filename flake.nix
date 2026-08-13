c75e2b2c07fbe8fa7290ae2b2f8230088f1ff18e
vs-123 <>
2026-08-13 06:47:51 +0000

    fix stylix palette
    - previously, i generated palette from wallpaper image
    - problem was that, when i'd make a tiny tweak to my stylix
      config and run nixos-rebuild, it'd download 3.2 GiB of stuff
      each time for haskell compilation
    - instead of that, i used nix repl and ran this:
      nixosConfigurations.karm.config.stylix.base16Scheme
    - this gave me the base16 scheme it generated, which i then pasted into
      my flake.nix
    - as a result, no more 3.2 GiB downloads

--- flake.nix
+++ flake.nix
@@ -18,7 +18,28 @@
         stylix.nixosModules.stylix {
           stylix = { 
             enable = true;
-            image = builtins.toPath self.wp-path;
+            #image = builtins.toPath self.wp-path;
+            base16Scheme = {
+              author = "Stylix";
+              base00 = "081d2e";
+              base01 = "004b72";
+              base02 = "366da6";
+              base03 = "72a3cc";
+              base04 = "a1bbdc";
+              base05 = "dae4ee";
+              base06 = "eaf2f4";
+              base07 = "eaf2f5";
+              base08 = "5c95cc";
+              base09 = "6896b7";
+              base0A = "7193c1";
+              base0B = "5797c5";
+              base0C = "7f93ac";
+              base0D = "6c97aa";
+              base0E = "6895be";
+              base0F = "7395ae";
+              scheme = "Stylix";
+              slug = "stylix";
+            };
             polarity = "dark";
           };
         }
